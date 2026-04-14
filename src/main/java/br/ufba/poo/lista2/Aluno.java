18d60eceb3e22c3aa362c5df9e3a815127c4bd1e
Jow <103865028+JoaoGomesss@users.noreply.github.com>
2026-04-14 19:39:16 -0300

    Implement Aluno class with attributes and methods

--- src/main/java/br/ufba/poo/lista2/Aluno.java
+++ src/main/java/br/ufba/poo/lista2/Aluno.java
@@ -1,22 +1,35 @@
-package br.ufba.poo.lista2;
-
-/**
- * Crie a classe `Aluno`, que possui os atributos `matricula` e `nome` (ambos do
- * tipo `String`), além dos métodos e construtores relevantes de acordo com a
- * seguinte especificação:
- *
- * - O nome do aluno pode ser alterado, mas nunca o seu número de matrícula
- * - O número de matrícula de um aluno deve poder ser acessado por outras partes
- * do programa
- * - Duas instâncias de `Aluno` são consideradas iguais se possuem o mesmo número
- * de matrícula
- *
- * Assuma, para este exercício, que a classe `Aluno` sempre será instanciada com um
- * número de matrícula e um nome válidos, passados nessa ordem.
- */
 public class Aluno {
+    private String nome;
+    private final String matricula;
 
-    public Aluno(String matricula, String nome) {
+    public Aluno(String matricula, String nome){
+        this.nome = nome;
+        this.matricula = matricula;
     }
-}
 
+    public String getNome(){
+        return  nome;
+    }
+
+    public String getMatricula(){
+        return matricula;
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    @Override
+    public boolean equals(Object obj){
+        if (obj == null){
+            return false;
+        } else if (!(obj instanceof Aluno)) {
+            return false;
+        }
+        Aluno aluno1 = (Aluno) obj;
+        return this.matricula.equals(aluno1.matricula);
+
+    }
+
+
+}
